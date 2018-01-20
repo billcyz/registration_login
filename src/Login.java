3425dd653f708655dd1408f655038b2db72e3d7c
billcyz <billcyz@gmail.com>
2018-01-20 22:45:14 +1300

    login basic search function

--- src/Login.java
+++ src/Login.java
@@ -8,6 +8,7 @@ import java.sql.*;
 
 public class Login extends HttpServlet {
 	
+//	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	long currentTime;
 	String userName;
 	String password;
@@ -16,30 +17,43 @@ public class Login extends HttpServlet {
 		currentTime = new TimeStamp().getEpochTime();
 	}
 	
-	public void doGet(HttpServletRequest request, 
-			HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		response.setCharacterEncoding("UTF-8");
-		PrintWriter out = response.getWriter();
-		
-		out.println("!!!!!!");
-	}
+//	public void doGet(HttpServletRequest request, 
+//			HttpServletResponse response) throws ServletException, IOException {
+//		response.setContentType("text/html");
+//		response.setCharacterEncoding("UTF-8");
+//		PrintWriter out = response.getWriter();
+//		
+//		out.println("!!!!!!");
+//	}
 	
 	// Handle POST request
 	public void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		
-		userName = request.getParameter("username");
-		password = request.getParameter("password");
-		
 		response.setContentType("text/html");
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
 		
+		userName = request.getHeader("username");
+		password = request.getHeader("password");
+		
+		
+		out.println("user name is: " + userName + ", " + "password is: " + password);
+		
+//		boolean a = userLogin("user_info", userName, password);
+		
+//		out.println("Result is: " + a);
+		
 		// DB interaction
-		if(!userLogin("user_info", userName, password)) {	
-			out.println("Error");
+		if(!userLogin("user_info", userName, password)) {
+			// User not found
+			
+			out.println(userName + ", " + password);
+			out.println("Result is: Error");
 		} else {
+			// Find user
+			
+			
 			out.println("Found User");
 		}
 	}
@@ -48,28 +62,28 @@ public class Login extends HttpServlet {
 	// Require table name, column name, user name, and user password
 	public boolean userLogin(String tableName, String name, String pwd) {
 		try {
-			DbConnection con = new DbConnection("jdbc:mysql://127.0.0.1:3306", "br_test",
-					"root", "Cyz37212302494");
+			System.out.println("tablename is: " + tableName + ", " + "name is: " + name + ", " + "pwd is: " + pwd);
+			DbConnection con = new DbConnection("jdbc:mysql://192.168.146.132:3306", "br_test",
+					"root", "password");
 			// prepared statement
-			String preparedStmt = "SELECT `name` FROM ? WHERE `name` = ?"
+			String preparedStmt = "SELECT `name` FROM " + tableName + " WHERE `name` = ?"
 					+ " and `password` = ?";
 			Class.forName(DbConnection.JDBC_DRIVER);
 			Connection conn = DriverManager.getConnection(con.DB_URL + "/" + con.DB, con.USER, con.PASS);
 			
 			PreparedStatement pstmt = conn.prepareStatement(preparedStmt);
-			pstmt.setString(1, tableName);
-			pstmt.setString(2, name);
-			pstmt.setString(3, pwd);
+			pstmt.setString(1, name);
+			pstmt.setString(2, pwd);
 			
 			ResultSet rs = pstmt.executeQuery();
 			
 			if(rs.next()) {
-				do {
-					System.out.println("NAME: " + rs.getString("name"));
-				} while(rs.next());
+//				do {
+////					System.out.println("NAME: " + rs.getString("name"));
+//				} while(rs.next());
 				return true;
 			} else {
-				System.out.println("No data");
+//				System.out.println("No data");
 				return false;
 			}
 		} catch (SQLException se){
